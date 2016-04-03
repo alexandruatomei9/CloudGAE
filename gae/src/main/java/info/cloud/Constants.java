78539dc2029b269e8b1b4877ff1bfc7427735586
Alexandru Atomei <alexandruatomei9@gmail.com>
2016-04-03 13:54:17 +0300

    added google calendar

--- gae/src/main/java/info/cloud/Constants.java
+++ gae/src/main/java/info/cloud/Constants.java
@@ -4,7 +4,7 @@ package info.cloud;
  * Contains the client IDs and scopes for allowed clients consuming the helloworld API.
  */
 public class Constants {
-  public static final String WEB_CLIENT_ID = "396486651427-9v868j75sdsmjjc45qjvng3a3lhhhu4s.apps.googleusercontent.com";
+  public static final String WEB_CLIENT_ID = "396486651427-6oja5nn04gfo35d7e8s2iegu37mnqsjf.apps.googleusercontent.com";
   public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
   public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
   public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
